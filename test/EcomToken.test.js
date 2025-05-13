5d8e21b47c81b75d2773dbc6b27d6ad600f4526d
Kartikey Dubey <kartikeykv2@gmail.com>
2025-05-13 16:30:14 +0530

    Update EcomToken.test.js

--- test/EcomToken.test.js
+++ test/EcomToken.test.js
@@ -3,21 +3,51 @@ const { ethers } = require("hardhat");
 
 describe("EComToken", function () {
   let token;
-  let deployer;
+  let owner, merchant, user;
 
   beforeEach(async () => {
-    [deployer] = await ethers.getSigners();
+    [owner, merchant, user] = await ethers.getSigners();
     const EComToken = await ethers.getContractFactory("EComToken");
-    token = await EComToken.deploy();
+    token = await EComToken.deploy(owner.address);
+    await token.waitForDeployment();
+  });
+
+  it("Should deploy with correct parameters", async () => {
+    expect(await token.name()).to.equal("ECommerce Global Token");
+    expect(await token.symbol()).to.equal("ECOM");
+
+    const totalSupply = ethers.parseUnits("100000000", 18);
+    expect(await token.totalSupply()).to.equal(totalSupply);
+    expect(await token.balanceOf(owner.address)).to.equal(totalSupply);
+  });
+
+  it("Should handle fee calculations correctly", async () => {
+    await token.setTransactionFee(100); // 1%
+
+    const initialBalance = await token.balanceOf(owner.address);
+
+    await token.transfer(user.address, 1000);
+
+    const finalBalance = await token.balanceOf(owner.address);
+
+    expect(finalBalance).to.equal(initialBalance - 1000n + 10n);
+  });
+
+  it("Should prevent blacklisted addresses", async () => {
+    await token.setBlacklist(user.address, true);
+
+    await expect(
+      token.connect(user).transfer(owner.address, 100)
+    ).to.be.revertedWithCustomError(token, "Blacklisted");
+  });
+
+  it("Should allow merchant whitelisting", async () => {
+    await token.whitelistMerchant(merchant.address);
+    expect(await token.whitelistedMerchants(merchant.address)).to.be.true;
   });
 
   it("Should have correct name and symbol", async () => {
-    expect(await token.name()).to.equal("ECom Token");
+    expect(await token.name()).to.equal("ECommerce Global Token");
     expect(await token.symbol()).to.equal("ECOM");
-  });
-
-  it("Should mint initial supply to deployer", async () => {
-    const totalSupply = await token.totalSupply();
-    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
   });
 });
